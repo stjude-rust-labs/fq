bf62e0763b40504ba25c2b1f755bfecc64402356
Michael Macias <michael.macias@stjude.org>
2018-05-20 14:57:22 -0500

    Use lint mode and validation levels

--- src/bin/fqlint.rs
+++ src/bin/fqlint.rs
@@ -3,7 +3,11 @@ extern crate fqlib;
 
 use clap::{App, Arg};
 use fqlib::PairedFastQReader;
-use fqlib::validators::validate_pair;
+use fqlib::validators::{self, BlockValidator, LintMode, ValidationLevel};
+
+fn report_error(error: validators::Error) {
+    println!("{:?}", error);
+}
 
 fn main() {
     let matches = App::new("fqlint")
@@ -11,6 +15,16 @@ fn main() {
         .arg(Arg::with_name("lint-mode")
              .long("lint-mode")
              .value_name("MODE")
+             .possible_values(&["error", "report"])
+             .default_value("error"))
+        .arg(Arg::with_name("single-read-validation-level")
+             .long("single-read-validation-level")
+             .value_name("LEVEL")
+             .possible_values(&["minimum", "low", "high"])
+             .default_value("high"))
+        .arg(Arg::with_name("paired-read-validation-level")
+             .long("paired-read-validation-level")
+             .value_name("LEVEL")
              .possible_values(&["minimum", "low", "high"])
              .default_value("high"))
         .arg(Arg::with_name("r1-input-pathname")
@@ -26,15 +40,39 @@ fn main() {
     let r1_input_pathname = matches.value_of("r1-input-pathname").unwrap();
     let r2_input_pathname = matches.value_of("r2-input-pathname").unwrap();
 
+    let lint_mode = value_t!(matches, "lint-mode", LintMode).unwrap_or_else(|e| e.exit());
+
+    let single_read_validation_level = value_t!(
+        matches,
+        "single-read-validation-level",
+        ValidationLevel
+    ).unwrap_or_else(|e| e.exit());
+
+    let paired_read_validation_level = value_t!(
+        matches,
+        "paired-read-validation-level",
+        ValidationLevel
+    ).unwrap_or_else(|e| e.exit());
+
     let reader = PairedFastQReader::open(
         r1_input_pathname,
         r2_input_pathname,
     ).unwrap();
 
+    let validator = BlockValidator::new(
+        single_read_validation_level,
+        paired_read_validation_level,
+    );
+
     for (r1_block, r2_block) in reader {
         let b = r1_block.unwrap();
         let d = r2_block.unwrap();
 
-        validate_pair(&b, &d).unwrap();
+        if let Err(e) = validator.validate_pair(&b, &d) {
+            match lint_mode {
+                LintMode::Error => panic!("{:?}", e),
+                LintMode::Report => report_error(e),
+            }
+        }
     }
 }
--- src/validators/mod.rs
+++ src/validators/mod.rs
@@ -20,6 +20,7 @@ pub enum Error {
     Invalid(String),
 }
 
+#[derive(Debug, Eq, PartialEq)]
 pub enum LintMode {
     Report,
     Error,
@@ -37,7 +38,7 @@ impl FromStr for LintMode {
     }
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub enum ValidationLevel {
     Minimum,
     Low,
@@ -57,27 +58,58 @@ impl FromStr for ValidationLevel {
     }
 }
 
-fn validate(b: &Block) -> Result<(), Error> {
-    lazy_static! {
-        static ref NAME_VALIDATOR: NameValidator = NameValidator;
-        static ref COMPLETE_VALIDATOR: CompleteValidator = CompleteValidator;
-        static ref ALPHABET_VALIDATOR: AlphabetValidator = Default::default();
-        static ref PLUS_LINE_VALIDATOR: PlusLineValidator = PlusLineValidator;
-    }
-
-    PLUS_LINE_VALIDATOR.validate(b)?;
-    ALPHABET_VALIDATOR.validate(b)?;
-    NAME_VALIDATOR.validate(b)?;
-    COMPLETE_VALIDATOR.validate(b)
+pub struct BlockValidator {
+    single_read_validation_level: ValidationLevel,
+    paired_read_validation_level: ValidationLevel,
 }
 
-pub fn validate_pair(b: &Block, d: &Block) -> Result<(), Error> {
-    lazy_static! {
-        static ref NAMES_VALIDATOR: NamesValidator = NamesValidator;
+impl BlockValidator {
+    pub fn new(
+        single_read_validation_level: ValidationLevel,
+        paired_read_validation_level: ValidationLevel,
+    ) -> BlockValidator {
+        BlockValidator {
+            single_read_validation_level,
+            paired_read_validation_level,
+        }
     }
 
-    validate(b)?;
-    validate(d)?;
+    pub fn validate(&self, b: &Block) -> Result<(), Error> {
+        lazy_static! {
+            static ref NAME_VALIDATOR: NameValidator = NameValidator;
+            static ref COMPLETE_VALIDATOR: CompleteValidator = CompleteValidator;
+            static ref ALPHABET_VALIDATOR: AlphabetValidator = Default::default();
+            static ref PLUS_LINE_VALIDATOR: PlusLineValidator = PlusLineValidator;
+        }
 
-    NAMES_VALIDATOR.validate(b, d)
+        if self.single_read_validation_level >= ValidationLevel::Minimum {
+            PLUS_LINE_VALIDATOR.validate(b)?;
+            COMPLETE_VALIDATOR.validate(b)?;
+
+            if self.single_read_validation_level >= ValidationLevel::Low {
+                ALPHABET_VALIDATOR.validate(b)?;
+
+                if self.single_read_validation_level >= ValidationLevel::High {
+                    NAME_VALIDATOR.validate(b)?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn validate_pair(&self, b: &Block, d: &Block) -> Result<(), Error> {
+        lazy_static! {
+            static ref NAMES_VALIDATOR: NamesValidator = NamesValidator;
+        }
+
+        self.validate(b)?;
+        self.validate(d)?;
+
+        if self.paired_read_validation_level >= ValidationLevel::Low {
+            NAMES_VALIDATOR.validate(b, d)?;
+        }
+
+        Ok(())
+    }
 }
