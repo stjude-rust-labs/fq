94aa428e7b4ff9bfdfb257e31f3ed040cf9bc1d9
Michael Macias <michael.macias@stjude.org>
2018-11-02 09:14:27 -0500

    Initialize logger from RUST_LOG if set
    Users should typically use the `--verbose` flag to enable info level
    logging, but `RUST_LOG` can be set for more granular control, e.g., for
    debugging.

--- src/main.rs
+++ src/main.rs
@@ -74,6 +74,8 @@ fn main() {
         env_logger::Builder::from_default_env()
             .filter(Some(crate_name!()), LevelFilter::Info)
             .init();
+    } else {
+        env_logger::init();
     }
 
     if let Some(m) = matches.subcommand_matches("generate") {
