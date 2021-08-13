ff69a799c886b1ea1bc87eecc6ee75c4b544b796
Michael Macias <michael.macias@stjude.org>
2021-08-13 11:47:48 -0500

    fastq: Fix typo

--- src/fastq.rs
+++ src/fastq.rs
@@ -36,11 +36,11 @@ where
     P: AsRef<Path>,
 {
     let path = src.as_ref();
-    let extenstion = path.extension();
+    let extension = path.extension();
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
-    match extenstion.and_then(|ext| ext.to_str()) {
+    match extension.and_then(|ext| ext.to_str()) {
         Some("gz") => {
             let decoder = MultiGzDecoder::new(reader);
             Ok(Reader::new(Box::new(BufReader::new(decoder))))
