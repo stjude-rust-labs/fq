12dd9a0b3daad3ad679ad605b659a68b349b7a45
Michael Macias <michael.macias@stjude.org>
2023-08-24 14:02:24 -0500

    commands/generate: Add generate error

--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -1,10 +1,12 @@
-use anyhow::Context;
+use std::{io, path::PathBuf};
+
 use rand::{rngs::SmallRng, SeedableRng};
+use thiserror::Error;
 use tracing::info;
 
 use crate::{cli::GenerateArgs, generator::Builder, Generator, PairWriter};
 
-pub fn generate(args: GenerateArgs) -> anyhow::Result<()> {
+pub fn generate(args: GenerateArgs) -> Result<(), GenerateError> {
     info!("fq-generate start");
 
     let builder = if let Some(seed) = args.seed {
@@ -19,22 +21,27 @@ pub fn generate(args: GenerateArgs) -> anyhow::Result<()> {
 
     let generator = builder.set_read_length(args.read_length).build();
 
-    let w1 = crate::fastq::create(r1_dst)
-        .with_context(|| format!("Could not create file: {}", r1_dst.display()))?;
-
-    let w2 = crate::fastq::create(r2_dst)
-        .with_context(|| format!("Could not create file: {}", r2_dst.display()))?;
+    let w1 =
+        crate::fastq::create(r1_dst).map_err(|e| GenerateError::CreateFile(e, r1_dst.clone()))?;
+    let w2 =
+        crate::fastq::create(r2_dst).map_err(|e| GenerateError::CreateFile(e, r2_dst.clone()))?;
 
     let mut writer = PairWriter::new(w1, w2);
 
     let record_count = args.record_count;
 
-    writer
-        .write(generator, record_count)
-        .context("Could not write generated records")?;
+    writer.write(generator, record_count)?;
 
     info!("generated {} records", record_count);
     info!("fq-generate end");
 
     Ok(())
+}
+
+#[derive(Debug, Error)]
+pub enum GenerateError {
+    #[error("I/O error")]
+    Io(#[from] io::Error),
+    #[error("could not create file: {1}")]
+    CreateFile(#[source] io::Error, PathBuf),
 }
