0aa42ffd8bb7482947939dfd5a2004b6492cd9d3
Michael Macias <michael.macias@stjude.org>
2024-07-31 15:37:55 -0500

    distributions/character: Move doctest to tests

--- src/distributions/character.rs
+++ src/distributions/character.rs
@@ -4,19 +4,6 @@ use rand::{
 };
 
 /// Sample a `char`, uniformly distributed over a given character set.
-///
-/// # Examples
-///
-/// ```
-/// use rand::{Rng, thread_rng};
-/// use fq::distributions::Character;
-///
-/// let mut rng = thread_rng();
-/// let distribution = Character::new(b"AGTC");
-/// let bytes: Vec<u8> = rng.sample_iter(&distribution).take(8).collect();
-/// let s = String::from_utf8(bytes).unwrap();
-/// println!("{}", s); // e.g., "TCCTCGAG"
-/// ```
 pub struct Character {
     alphabet: &'static [u8],
     range: Uniform<usize>,
@@ -44,9 +31,13 @@ mod tests {
 
     #[test]
     fn test_sample() {
-        let distribution = Character::new(b"abcd");
         let mut rng = StepRng::new(0, 1);
-        let x = rng.sample(distribution);
-        assert_eq!(x, b'a');
+        let distribution = Character::new(b"ACGT");
+
+        let c = rng.sample(&distribution);
+        assert_eq!(c, b'A');
+
+        let buf: Vec<u8> = rng.sample_iter(&distribution).take(3).collect();
+        assert_eq!(buf, b"AAA");
     }
 }
