a24ce9b9d74352a813b469231d8fd029546307e7
Michael Macias <michael.macias@stjude.org>
2024-07-31 16:49:27 -0500

    validators: Only display message in error

--- src/validators.rs
+++ src/validators.rs
@@ -66,7 +66,7 @@ impl Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "[{}] {}: {}", self.code, self.name, self.error)
+        self.error.fmt(f)
     }
 }
 
