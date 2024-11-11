c5af096110940b660cec498a9500ab8a832346d2
Michael Macias <michael.macias@stjude.org>
2024-11-11 13:28:51 -0600

    commands/describe: Track min and max sequence lengths

--- src/commands/describe.rs
+++ src/commands/describe.rs
@@ -16,13 +16,26 @@ pub fn describe(args: DescribeArgs) -> io::Result<()> {
     }
 
     println!("record_count\t{}", metrics.record_count);
+    println!("min_sequence_length\t{}", metrics.min_sequence_length);
+    println!("max_sequence_length\t{}", metrics.max_sequence_length);
 
     Ok(())
 }
 
-#[derive(Default)]
 struct Metrics {
     record_count: u64,
+    min_sequence_length: usize,
+    max_sequence_length: usize,
+}
+
+impl Default for Metrics {
+    fn default() -> Self {
+        Self {
+            record_count: 0,
+            min_sequence_length: usize::MAX,
+            max_sequence_length: usize::MIN,
+        }
+    }
 }
 
 fn visit(metrics: &mut Metrics, _: &Record) {
