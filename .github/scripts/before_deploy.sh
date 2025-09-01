cfac6f7da9e99d9a213701c7ee87c461c5742955
Michael Macias <michael.macias@stjude.org>
2025-09-01 14:16:35 -0500

    github/scripts/before_deploy: Print archive pathname as output

--- .github/scripts/before_deploy.sh
+++ .github/scripts/before_deploy.sh
@@ -3,7 +3,7 @@
 set -ex
 
 main() {
-    local package_name="$CRATE_NAME-${GITHUB_REF/refs\/tags\/v/}-$TARGET"
+    local package_name="$CRATE_NAME-${GITHUB_REF_NAME#v}-$TARGET"
 
     local staging_prefix
     staging_prefix=$(mktemp -d)
@@ -18,17 +18,21 @@ main() {
     cp "target/$TARGET/release/$CRATE_NAME" "$working_prefix"
     cp LICENSE.txt README.md "$working_prefix"
 
-    pushd "$staging_prefix"
+    pushd "$staging_prefix" 1>&2
 
     if [[ $OS_NAME == windows-* ]]; then
-        7z a "$dst_prefix/$package_name.zip" "$package_name"
+        dst="$dst_prefix/$package_name.zip"
+        7z a "$dst" "$package_name" 1>&2
     else
-        tar cfz "$dst_prefix/$package_name.tar.gz" "$package_name"
+        dst="$dst_prefix/$package_name.tar.gz"
+        tar cfz "$dst" "$package_name"
     fi
 
-    popd
+    popd 1>&2
 
     rm -r "$staging_prefix"
+
+    echo "$dst"
 }
 
 main
