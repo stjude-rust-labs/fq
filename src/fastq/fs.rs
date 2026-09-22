92538a4d572a0dc3bb5f024e28f6fa2b773ded22
Michael Macias <michael.macias@stjude.org>
2026-09-22 16:01:34 -0500

    fs: Extract creating a maybe gzip-compressed file

--- src/fastq/fs.rs
+++ src/fastq/fs.rs
@@ -1,31 +1,16 @@
 use std::{
-    fs::File,
-    io::{self, BufWriter, Read, Write},
+    io::{self, Read, Write},
     path::Path,
 };
 
-use flate2::{Compression, write::GzEncoder};
-
 use super::io::{Reader, Writer};
-use crate::fs::open_raw_or_gz;
-
-const GZ_EXTENSION: &str = "gz";
+use crate::fs::{create_raw_or_gz, open_raw_or_gz};
 
 pub fn create<P>(dst: P) -> io::Result<Writer<Box<dyn Write>>>
 where
     P: AsRef<Path>,
 {
-    let path = dst.as_ref();
-    let writer = File::create(path).map(BufWriter::new)?;
-
-    match path.extension().and_then(|ext| ext.to_str()) {
-        Some(GZ_EXTENSION) => {
-            let level = Compression::default();
-            let encoder = GzEncoder::new(writer, level);
-            Ok(Writer::new(Box::new(encoder)))
-        }
-        _ => Ok(Writer::new(Box::new(writer))),
-    }
+    create_raw_or_gz(dst).map(Writer::new)
 }
 
 pub fn open<P>(src: P) -> io::Result<Reader<Box<dyn Read>>>
--- src/fs.rs
+++ src/fs.rs
@@ -1,12 +1,29 @@
 use std::{
     fs::File,
-    io::{self, Read},
+    io::{self, BufWriter, Read, Write},
     path::Path,
 };
 
-use flate2::read::MultiGzDecoder;
+use flate2::{Compression, read::MultiGzDecoder, write::GzEncoder};
 
 const GZ_EXTENSION: &str = "gz";
+
+pub fn create_raw_or_gz<P>(dst: P) -> io::Result<Box<dyn Write>>
+where
+    P: AsRef<Path>,
+{
+    let path = dst.as_ref();
+    let writer = File::create(path).map(BufWriter::new)?;
+
+    match path.extension().and_then(|ext| ext.to_str()) {
+        Some(GZ_EXTENSION) => {
+            let level = Compression::default();
+            let encoder = GzEncoder::new(writer, level);
+            Ok(Box::new(encoder))
+        }
+        _ => Ok(Box::new(writer)),
+    }
+}
 
 pub fn open_raw_or_gz<P>(src: P) -> io::Result<Box<dyn Read>>
 where
